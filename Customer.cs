755caf43638c74a991d99c7fa9dce655acde385c
thiagocruzsoares <thiagocsoares@outlook.com>
2016-07-13 16:24:03 -0300

    good

--- Customer.cs
+++ Customer.cs
@@ -3,13 +3,9 @@ public class Customer
 	string name;
 	int value;
 	string aa;
-<<<<<<< HEAD
-	
+	string ronaldo = "bri";
 	private void GetCustomerByID(int idCustomer)
 	{
 		//treinamento
 	}
-=======
-	string ronaldo = "bri";
->>>>>>> de182c37bddb4f17e95b6fec4e7cfc7707b9d127
 }
